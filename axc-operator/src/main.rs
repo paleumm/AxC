cbede273317af72b229fad3f43e0b8d46efa6694
Permpoon Boonyarit <zdotqdot@gmail.com>
2022-09-17 14:20:21 +0700

    Update main.rs

--- axc-operator/src/main.rs
+++ axc-operator/src/main.rs
@@ -1,7 +1,39 @@
-use kube::Client;
-use serde_json::{Result, Value};
+use k8s_openapi::api::core::v1::Pod;
+use kube::{
+    api::{Api, DeleteParams, ListParams, Patch, PatchParams, PostParams, ResourceExt},
+    runtime::wait::{await_condition, conditions::is_pod_running},
+    Client,
+};
+use serde_json::json;
+use tracing::*;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> anyhow::Result<()> {
     let client = Client::try_default().await.unwrap();
+    let pods: Api<Pod> = Api::default_namespaced(client);
+
+    let p: Pod = serde_json::from_value(json!({
+        "apiVersion": "v1",
+        "kind": "Pod",
+        "metadata": { "name": "blog" },
+        "spec": {
+            "containers": [{
+              "name": "blog",
+              "image": "clux/blog:0.1.0"
+            }],
+        }
+    }))?;
+
+    let pp = PostParams::default();
+    match pods.create(&pp, &p).await {
+        Ok(o) => {
+            let name = o.name_any();
+            assert_eq!(p.name_any(), name);
+            info!("Created {}", name);
+        }
+        Err(kube::Error::Api(ae)) => assert_eq!(ae.code, 409), // if you skipped delete, for instance
+        Err(e) => return Err(e.into()),                        // any other case is probably bad
+    }
+
+    Ok(())
 }
