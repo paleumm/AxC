4e6b78c4f8116c1ac3b89252eb000364b60ba12b
Permpoon Boonyarit <zdotqdot@gmail.com>
2022-09-18 20:09:21 +0700

    Update main.rs

--- axc-controller/src/main.rs
+++ axc-controller/src/main.rs
@@ -16,7 +16,8 @@ async fn main() -> anyhow::Result<()> {
     let client = Client::try_default().await?;
     let pods: Api<Pod> = Api::default_namespaced(client);
 
-    axc_controller::create_runtime(&pods).await.unwrap();
+    // axc_controller::create_runtime(&pods).await.unwrap();
+    axc_controller::delete_pod("blog", pods).await;
 
     Ok(())
 }
