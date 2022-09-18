14940685e6881cb5b424e3d11637694cdbfc30ee
paleumm <zdotqdot@gmail.com>
2022-09-18 19:55:48 +0700

    Update main.rs

--- axc-controller/src/main.rs
+++ axc-controller/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
+// use k8s_openapi::api::core::v1::Pod;
+// use serde_json::json;
+// use tracing::*;
+
+// use kube::{
+//     api::{Api, DeleteParams, ListParams, Patch, PatchParams, PostParams, ResourceExt},
+//     runtime::wait::{await_condition, conditions::is_pod_running},
+//     Client,
+// };
+
+use kube::Client;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
     println!("Hello, world!");
+    let client = Client::try_default().await?;
+
+    axc_controller::create_runtime(client).await.unwrap();
+
+    Ok(())
 }
